6cda81fba15f66b1f0b8c1e8d7eb78646391e6c6
Furkan Kadir Güzeloğlu <74448882+furkankadirguzeloglu@users.noreply.github.com>
2024-04-13 21:48:30 +0300

    Added devBuilds_openssl_zlib_curl

---
+++ demo/devBuilds_openssl_zlib_curl/devBuilds/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+#include <openSSL/ssl.h>
+#include <zLib/zlib.h>
+#include <cURL/curl.h>
+#pragma comment(lib, "libcrypto.lib")
+#pragma comment(lib, "libssl.lib")
+#pragma comment(lib, "zdll.lib")
+#pragma comment(lib, "zlib.lib")
+#pragma comment(lib, "libcurl.lib")
+
+int main(int argc, char* argv[]) {
+    return EXIT_SUCCESS;
+}
